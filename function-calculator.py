33ec3aa5bd493eb293b33d5a777a29a53a5c2dc1
Ionlylike1kindofcheese <99066333@mydavinci.nl>
2021-10-27 11:14:50 +0200

    Programma 2 van opdracht: F1.5.01.L3 - Tri-Tryout

---
+++ function-calculator.py
@@ -0,0 +1,48 @@
+def addition(number1, number2):
+    return number1 + number2
+
+
+def subtraction(number1, number2):
+    return number1 - number2
+
+
+def multiplication(number1, number2):
+    return number1 * number2
+
+
+def division(number1, number2):
+    return number1 / number2
+
+
+def increment(number):
+    return number + 1
+
+
+def decrement(number):
+    return number - 1
+
+print("----------------")
+print("19 + 13 =", str(addition(19, 13)))
+print("45 + 18 =", str(addition(45, 18)))
+print("26 + 43 =", str(addition(26, 43)))
+print("----------------")
+print("73 - 14 =", str(subtraction(73, 14)))
+print("56 - 17 =", str(subtraction(56, 17)))
+print("34 - 15 =", str(subtraction(34, 15)))
+print("----------------")
+print("5 x 13 =", str(multiplication(5, 13)))
+print("3 x 12 =", str(multiplication(3, 12)))
+print("9 x 18 =", str(multiplication(9, 18)))
+print("----------------")
+print("32 : 4 =", str(division(32, 4)))
+print("40 : 5 =", str(division(40, 5)))
+print("64 : 8 =", str(division(64, 8)))
+print("----------------")
+print("13 + 1 =", str(increment(13)))
+print("68 + 1 =", str(increment(68)))
+print("59 + 1 =", str(increment(59)))
+print("----------------")
+print("48 - 1 =", str(decrement(48)))
+print("37 - 1 =", str(decrement(37)))
+print("12 - 1 =", str(decrement(12)))
+print("----------------")
